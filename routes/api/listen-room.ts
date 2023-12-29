072e0a6aa37288ed83b89a8556d73c8199d4a5d0
David Lezama <85713808+IamDavidev@users.noreply.github.com>
2023-12-29 17:44:06 +0000

    Fix formatting in listen-room.ts

--- routes/api/listen-room.ts
+++ routes/api/listen-room.ts
@@ -1,31 +1,31 @@
-import { Handlers } from "$fresh/server.ts";
+import { Handlers } from '$fresh/server.ts'
 
-const WAIT_CHUNK = `retry: 1000\n\n`;
+const WAIT_CHUNK = `retry: 1000\n\n`
 
 export const handler: Handlers = {
-  GET(_req, ctx) {
-    const channel = new BroadcastChannel("editor-room");
+  GET(_req, _ctx) {
+    const channel = new BroadcastChannel('editor-room')
 
     const body = new ReadableStream({
       start(controller) {
-        controller.enqueue(WAIT_CHUNK);
-        channel.onmessage = (e) => {
-          const data = e;
-          controller.enqueue(`data: ${JSON.stringify(data)}\n\n`);
-        };
+        controller.enqueue(WAIT_CHUNK)
+        channel.onmessage = e => {
+          const data = e
+          controller.enqueue(`data: ${JSON.stringify(data)}\n\n`)
+        }
       },
       cancel() {
-        channel.close();
+        channel.close()
       },
-    });
+    })
 
-    const response = body.pipeThrough(new TextEncoderStream());
+    const response = body.pipeThrough(new TextEncoderStream())
 
     return new Response(response, {
       headers: {
-        "Content-Type": "text/event-stream",
-        "Cache-Control": "no-cache",
+        'Content-Type': 'text/event-stream',
+        'Cache-Control': 'no-cache',
       },
-    });
+    })
   },
-};
+}
