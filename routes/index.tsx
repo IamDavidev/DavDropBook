58dd43f4bcbd550f12360a3b0ee9647e46eae7a6
David Lezama <85713808+IamDavidev@users.noreply.github.com>
2023-12-29 17:36:07 +0000

    Add new routes and islands

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,25 +1,26 @@
-import { useSignal } from "@preact/signals";
-import Counter from "../islands/Counter.tsx";
+import { useSignal } from '@preact/signals'
+import Counter from '../islands/Counter.tsx'
 
 export default function Home() {
-  const count = useSignal(3);
+  const count = useSignal(3)
+
+  const classes = {
+    button:
+      'bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded',
+  }
+
+  const links = {
+    editor: {
+      href: '/editor',
+      text: 'Editor',
+    },
+  }
+
   return (
-    <div class="px-4 py-8 mx-auto bg-[#86efac]">
-      <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
-        <img
-          class="my-6"
-          src="/logo.svg"
-          width="128"
-          height="128"
-          alt="the Fresh logo: a sliced lemon dripping with juice"
-        />
-        <h1 class="text-4xl font-bold">Welcome to Fresh</h1>
-        <p class="my-4">
-          Try updating this message in the
-          <code class="mx-2">./routes/index.tsx</code> file, and refresh.
-        </p>
-        <Counter count={count} />
-      </div>
+    <div class='px-4 py-8 mx-auto bg-slate-900 min-h-screen w-full'>
+      <a class={classes.button} href={links.editor.href}>
+        {links.editor.text}
+      </a>
     </div>
-  );
+  )
 }
