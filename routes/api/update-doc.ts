0d0fff95e6027539e05e07e8f2fc6cdfb36bb5cc
David Lezama <85713808+IamDavidev@users.noreply.github.com>
2024-01-15 20:21:30 +0000

    Add roomId and connection to update-doc handler

--- routes/api/update-doc.ts
+++ routes/api/update-doc.ts
@@ -1,12 +1,19 @@
 import { Handlers } from '$fresh/server.ts'
+import { CHANNELS } from '../../constants/channel.const.ts'
 
 export const handler: Handlers = {
   async POST(req) {
-    const id = crypto.randomUUID()
     const body = await req.json()
     const document = body.document
+    const roomId = body.roomId
 
-    const bc = new BroadcastChannel('editor-room')
+    const id = crypto.randomUUID()
+
+    const connection = CHANNELS.NEW_PUBLIC_ROOM_CONNECTION(roomId)
+    const channelName = connection.getChannelName as string
+
+    const bc = new BroadcastChannel(channelName)
+
     bc.postMessage({
       document,
       id,
