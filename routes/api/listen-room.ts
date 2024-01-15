2306b50acf41c7f154d97354593b3666841d5e19
David Lezama <85713808+IamDavidev@users.noreply.github.com>
2024-01-15 02:56:18 +0000

    Refactor listen-room.ts to use new live connection

--- routes/api/listen-room.ts
+++ routes/api/listen-room.ts
@@ -1,26 +1,34 @@
 import { Handlers } from '$fresh/server.ts'
-import { EVENT_STREAM_OPTIONS } from '../../constants/event-stream-options.ts'
 
-const WAIT_CHUNK = `retry: 1000\n\n`
+import { getNewLiveConnection } from '../../api/services/get-new-live-connection.ts'
+import { CHANNELS } from '../../constants/channel.const.ts'
+import { EVENT_STREAM_OPTIONS } from '../../constants/event-stream-options.ts'
 
 export const handler: Handlers = {
   GET(_req, _ctx) {
-    const channel = new BroadcastChannel('editor-room')
+    // const channel = new BroadcastChannel('editor-room')
 
-    const body = new ReadableStream({
-      start(controller) {
-        controller.enqueue(WAIT_CHUNK)
-        channel.onmessage = e => {
-          const data = e
-          controller.enqueue(`data: ${JSON.stringify(data)}\n\n`)
-        }
-      },
-      cancel() {
-        channel.close()
-      },
+    // const body = new ReadableStream({
+    //   start(controller) {
+    //     controller.enqueue(WAIT_CHUNK)
+    //     channel.onmessage = e => {
+    //       const data = e
+    //       controller.enqueue(`data: ${JSON.stringify(data)}\n\n`)
+    //     }
+    //   },
+    //   cancel() {
+    //     channel.close()
+    //   },
+    // })
+
+    // const response = body.pipeThrough(new TextEncoderStream())
+
+    const connection = CHANNELS.NEW_PUBLIC_ROOM_CONNECTION('editor-room')
+    const channel = connection.getChannelName as string
+
+    const response = getNewLiveConnection({
+      nameChannel: channel,
     })
-
-    const response = body.pipeThrough(new TextEncoderStream())
 
     return new Response(response, EVENT_STREAM_OPTIONS)
   },
