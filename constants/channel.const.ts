cc7988261f85e15f65e4e972762f876063d13084
David Lezama <85713808+IamDavidev@users.noreply.github.com>
2024-01-15 02:56:11 +0000

    Add CHANNELS.NEW_PUBLIC_ROOM_CONNECTION constant

---
+++ constants/channel.const.ts
@@ -0,0 +1,11 @@
+import { Connection } from '../api/models/Connection.model.ts'
+
+export const CHANNELS = {
+  /**
+   * Create a new connection to a room
+   * @param {string} roomId  room id to create a new unique channel connection
+   * @returns  {string} channel name
+   */
+  NEW_PUBLIC_ROOM_CONNECTION: (roomId: string): Connection =>
+    Connection.createNewConnectionPublic(roomId),
+}
