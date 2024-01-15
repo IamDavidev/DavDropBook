43e2ec01fcc2d662a313cf54a111cb793bc74ec1
David Lezama <85713808+IamDavidev@users.noreply.github.com>
2024-01-15 20:28:35 +0000

    Add new API routes for creating and updating documents

--- constants/routes.const.ts
+++ constants/routes.const.ts
@@ -1,7 +1,31 @@
 export const ROUTES = {
   API: {
     DOCUMENT: {
-      NEW_ROOM: '/api/new-room',
+      /**
+       * Create a path to create a new room
+       *
+       * @returns {string} path to create a new room
+       */
+      NEW_ROOM: (): string => '/api/new-room',
+      /**
+       * Create a path to listen a room and establish a connection
+       *
+       * @param roomId room id to listen a room
+       * @returns {string} path to listen a room
+       */
+      LISTEN_ROOM: ({
+        roomId,
+        userId,
+      }: {
+        roomId: string
+        userId: string
+      }): string => `/api/listen-room?roomId=${roomId}&userId=${userId}`,
+
+      /**
+       * Create a path to update a document
+       * @returns {string} path to update a document
+       */
+      UPDATE: (): string => `/api/update-doc`,
     },
   },
   APP: {
