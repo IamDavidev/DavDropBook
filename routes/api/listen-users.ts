b4e17de7161f9d4e84e080a67f87226d35a86a23
David Lezama <85713808+IamDavidev@users.noreply.github.com>
2024-01-17 03:47:20 +0000

    Refactor listen-users.ts to use dynamic channel names

--- routes/api/listen-users.ts
+++ routes/api/listen-users.ts
@@ -1,15 +1,22 @@
 import { Handlers } from '$fresh/server.ts'
+import { getNewLiveConnection } from '../../api/services/get-new-live-connection.ts'
 import { CONNECTION } from '../../constants/connection.const.ts'
 import { EVENT_STREAM_OPTIONS } from '../../constants/event-stream-options.ts'
 
 export const handler: Handlers = {
   async GET(req, _ctx) {
     const body = await req.json()
-
     const { roomId } = body
 
-    const connection = CONNECTION.NEW_PUBLIC_ROOM_CONNECTION('user-')
+    const connection = CONNECTION.NEW_PUBLIC_ROOM_CONNECTION(
+      `${roomId}-user-positions`
+    )
+    const channelName = connection.getChannelName
 
-    return new Response('Hello World', EVENT_STREAM_OPTIONS)
+    const response = getNewLiveConnection({
+      nameChannel: channelName as string,
+    })
+
+    return new Response(response, EVENT_STREAM_OPTIONS)
   },
 }
