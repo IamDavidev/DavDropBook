4fa9a707cb2390418ffd87efb40d40efe9060ee0
David Lezama <85713808+IamDavidev@users.noreply.github.com>
2024-01-17 03:46:31 +0000

    Add connection constants file

--- constants/connection.const.ts
+++ constants/connection.const.ts
@@ -1,6 +1,6 @@
 import { Connection } from '../api/models/Connection.model.ts'
 
-export const CHANNELS = {
+export const CONNECTION = {
   /**
    * Create a new connection to a room
    * @param {string} roomId  room id to create a new unique channel connection
