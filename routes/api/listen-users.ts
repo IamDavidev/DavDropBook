a88f44d7d5fd0fb595362fdb7507fb82b3b5fd06
David Lezama <85713808+IamDavidev@users.noreply.github.com>
2024-01-17 03:46:58 +0000

    Add listen-users API endpoint

--- routes/api/listen-users.ts
+++ routes/api/listen-users.ts
@@ -1,8 +1,15 @@
 import { Handlers } from '$fresh/server.ts'
+import { CONNECTION } from '../../constants/connection.const.ts'
 import { EVENT_STREAM_OPTIONS } from '../../constants/event-stream-options.ts'
 
 export const handler: Handlers = {
-  GET(_req, _ctx) {
+  async GET(req, _ctx) {
+    const body = await req.json()
+
+    const { roomId } = body
+
+    const connection = CONNECTION.NEW_PUBLIC_ROOM_CONNECTION('user-')
+
     return new Response('Hello World', EVENT_STREAM_OPTIONS)
   },
 }
