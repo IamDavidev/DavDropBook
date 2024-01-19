96c5dbd329102a5a5f95fddc10d786b63ae55cad
David Lezama <85713808+IamDavidev@users.noreply.github.com>
2024-01-19 02:35:12 +0000

    Add user management functionality to UserBar component

--- islands/user/user-bar.tsx
+++ islands/user/user-bar.tsx
@@ -1,10 +1,46 @@
+import { useSignal } from "@preact/signals";
+import { useEffect } from 'preact/hooks'
+
 interface UserBarProps {
+  users: User[]
+}
+
+interface User {
   /**
-   * The animal associated(random animal) with the user.
+   * 
+   */
+  id: string
+
+  /**
+   * 
    */
   animal: string
 }
 
 export const UserBar = (props: UserBarProps) => {
+
+  const user = useSignal(null)
+
+  const updateANewUser = (): void => {
+    console.log('update user')
+  }
+
+  const removeAUser = (): void => {
+    console.log('remove user')
+  }
+
+  useEffect(() => {
+    /**
+     * Remove the user from the list of users
+     * when the user closes the tab
+     * or the browser.
+     */
+    document.addEventListener('beforeunload', removeAUser)
+
+    return () => {
+      document.removeEventListener('beforeunload', removeAUser)
+    }
+  }, [])
+
   return <div></div>
 }
