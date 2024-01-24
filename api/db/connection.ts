6549a8f7c4a13042bd62f0a17c74f4b134f05519
David Lezama <85713808+IamDavidev@users.noreply.github.com>
2024-01-24 20:06:55 +0000

    Add DBKV type and prefixDB.document function

--- api/db/connection.ts
+++ api/db/connection.ts
@@ -1,1 +1,12 @@
+export type DBKV = Deno.Kv
 export const dbKV = await Deno.openKv()
+
+export type Prefix = [string, string]
+
+export const prefixDB = {
+  /**
+   * Return a prefix for a document to be stored in the Deno KV store
+   * @param {string} documentId
+   */
+  document: (documentId: string): Prefix => ['document', documentId],
+}
