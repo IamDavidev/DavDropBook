7fe4961e88bc9e5babf901494acd98c0d12086a5
David Lezama <85713808+IamDavidev@users.noreply.github.com>
2024-01-27 15:24:23 +0000

    Refactor Either class and its subclasses

--- api/utils/either.ts
+++ api/utils/either.ts
@@ -1,44 +1,76 @@
-export class Right<T> {
-  readonly value: T
+/**
+ * Class Right
+ */
 
-  constructor(value: T) {
-    this.value = value
+/**
+ * Abstract class Value
+ * To be extended by Left and Right
+ * It's a class that represents a value of one of two possible types (a disjoint union).
+ */
+abstract class Value<T> {
+  constructor(private readonly value: T) {}
+
+  abstract isLeft(): boolean
+  abstract isRight(): boolean
+}
+
+/**
+ * Class Left
+ * can be used for failure
+ * Left: failure or incorrect
+ */
+class Left<T> extends Value<T> {
+  isLeft(): boolean {
+    return true
   }
 
-  isLeft(): this is Left<T> {
+  isRight(): boolean {
+    return false
+  }
+}
+
+/**
+ * Class Right
+ * Can be used for success
+ * Right: success or correct
+ */
+class Right<T> extends Value<T> {
+  isLeft(): boolean {
     return false
   }
 
-  isRight(): this is Right<T> {
+  isRight(): boolean {
     return true
   }
 }
 
-export class Left<T> {
-  readonly value: T
-
-  constructor(value: T) {
-    this.value = value
-  }
-
-  isLeft(): this is Left<T> {
-    return true
-  }
-
-  isRight(): this is Right<T> {
-    return false
-  }
-}
-
+/**
+ * Class Either
+ *
+ * It's a class that represents a value of one of two possible types (a disjoint union).
+ * An instance of either is an instance of either a left or a right.
+ * Left is used for "failure" and Right is used for "success".
+ *
+ */
 export class Either {
+  /**
+   * It's a factory method that takes a value and returns a Left.
+   */
   static left<T>(value: T): Left<T> {
     return new Left(value)
   }
 
+  /**
+   * It's a factory method that takes a value and returns a Right.
+   */
   static right<T>(value: T): Right<T> {
     return new Right(value)
   }
 
+  /***
+   * fromNullable
+   * It's a factory method that takes a nullable value and returns a Left if the value is null or undefined,
+   */
   static fromNullable<T>(value: T): Either {
     return value !== null && value !== undefined
       ? Either.right(value)
